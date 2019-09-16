53da3cdd50de5806d192ab5bcb2f2721af6119c4
vuongvinhvien <vuongvinhvien@gmail.com>
2019-09-16 09:35:03 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <hello-component msg="Welcome Vuejs Cli3" />
+    <hr />
+    <input type="text" v-model="name1" />
+    <p>{{ name1}}</p>
+    <button type="button" v-on:click="changeBtn">change</button>
+    <br />
+    <h5>Child</h5>
+    <top-component :name2="name1" v-on:changparent="name1 = $event" :resetAllchild="resetAll">
+      <p>slot hêre</p>
+    </top-component>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import top from "./Shared/Top.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    "hello-component": HelloWorld,
+    "top-component": top
+  },
+  data: function() {
+    return {
+      name1: "121"
+    };
+  },
+  methods: {
+    changeBtn: function() {
+      this.name1 = "changed";
+    },
+    resetAll: function() {
+      this.name1 = "";
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #130606;
+  margin-top: 50px;
 }
 </style>
---
+++ src/Shared/Top.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>{{nameTop}}</h1>
+    <h1>{{name2}}</h1>
+    <input type="text" v-model="name2" />
+    <button @click="resetParent">Change from child</button>
+    <button type="button" v-on:click="resetAllchild">REset all</button>
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  // name: 'top',
+  data: function() {
+    return {
+      nameTop: "Name 's the first"
+    };
+  },
+  // props: ["name2"],
+  props: {
+    name2: {},
+    resetAllchild: Function
+  },
+  methods: {
+    resetParent: function() {
+      //   this.name2 = "reseted"
+      this.$emit("changparent", this.name2);
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
